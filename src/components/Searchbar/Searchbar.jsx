d6247b42dc34d2c4c21557b09635da5998b97626
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2023-05-03 21:32:04 +0300

    refactoring searchbar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -5,7 +5,7 @@ import {
   SearchForm,
   SearchInput,
 } from './Searchbar.styled';
-import { Component } from 'react';
+import { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {
@@ -13,46 +13,88 @@ import {
   notificationOptions,
 } from '../Notification/Notification';
 
-export class Searchbar extends Component {
-  state = {
-    textQuery: '',
-  };
-  // следим за изменениями Input (контролируемый элемент)
-  onChangeInput = e => {
-    this.setState({ textQuery: e.currentTarget.value.trim().toLowerCase() });
+export const Searchbar = ({ onSubmit }) => {
+  const [textQuery, setTextQuery] = useState('');
+
+  const onChangeInput = e => {
+    setTextQuery(e.currentTarget.value.trim().toLowerCase());
   };
 
-  onSubmitForm = e => {
+  const onSubmitForm = e => {
     e.preventDefault();
     // уведомление
-    if (this.state.textQuery === '') {
+    if (textQuery === '') {
       toast.error(`${notificationMassege}`, notificationOptions);
     }
     //фун-я onSubmit пришла из App через пропсы
-    this.props.onSubmit(this.state.textQuery);
+    onSubmit(textQuery);
     //очистка строки поиска
-    this.setState({ textQuery: '' });
+    setTextQuery('');
   };
 
-  render() {
-    return (
-      <SearchbarWrap>
-        <SearchForm onSubmit={this.onSubmitForm}>
-          <SearchBtn type="submit">
-            <BiSearch size="24" />
-          </SearchBtn>
+  return (
+    <SearchbarWrap>
+      <SearchForm onSubmit={onSubmitForm}>
+        <SearchBtn type="submit">
+          <BiSearch size="24" />
+        </SearchBtn>
 
-          <SearchInput
-            value={this.state.textQuery}
-            onChange={this.onChangeInput}
-            type="text"
-            autocomplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </SearchForm>
-        <ToastContainer />
-      </SearchbarWrap>
-    );
-  }
-}
+        <SearchInput
+          value={textQuery}
+          onChange={onChangeInput}
+          type="text"
+          autocomplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </SearchForm>
+      <ToastContainer />
+    </SearchbarWrap>
+  );
+};
+
+// Пример на классах
+
+// export class Searchbar extends Component {
+//   state = {
+//     textQuery: '',
+//   };
+//   // следим за изменениями Input (контролируемый элемент)
+//   onChangeInput = e => {
+//     this.setState({ textQuery: e.currentTarget.value.trim().toLowerCase() });
+//   };
+
+//   onSubmitForm = e => {
+//     e.preventDefault();
+//     // уведомление
+//     if (this.state.textQuery === '') {
+//       toast.error(`${notificationMassege}`, notificationOptions);
+//     }
+//     //фун-я onSubmit пришла из App через пропсы
+//     this.props.onSubmit(this.state.textQuery);
+//     //очистка строки поиска
+//     this.setState({ textQuery: '' });
+//   };
+
+//   render() {
+//     return (
+//       <SearchbarWrap>
+//         <SearchForm onSubmit={this.onSubmitForm}>
+//           <SearchBtn type="submit">
+//             <BiSearch size="24" />
+//           </SearchBtn>
+
+//           <SearchInput
+//             value={this.state.textQuery}
+//             onChange={this.onChangeInput}
+//             type="text"
+//             autocomplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//           />
+//         </SearchForm>
+//         <ToastContainer />
+//       </SearchbarWrap>
+//     );
+//   }
+// }
